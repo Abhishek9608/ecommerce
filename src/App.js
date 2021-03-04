1e4bf61317013e344c24086e871261e0abeb35cc
ABHISHEK KUMAR <amreshk005@gmail.com>
2021-03-04 14:33:57 +0530

    structured code

--- src/App.js
+++ src/App.js
@@ -3,15 +3,45 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./component/Home/Home";
 import Login from "./component/SignIn/Login";
 import Signup from "./component/Signup/Signup";
+import { MuiThemeProvider, createMuiTheme, withStyles } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  typography: {
+    h2: {
+      font: "normal normal 2.5em/1.143em 'P22StGSchriftPro',Helvetica, Arial, 'Lucida Grande', sans-serif",
+      marginBottom: "20px",
+      paddingBottom: "25px",
+      position: "relative",
+      textAlign: "center",
+      fontSize: "2.75rem",
+    },
+    body1: {
+      borderBottom: "1px solid #00040a",
+      color: "#00040a",
+      font: "600 normal 0.857em/1em 'News Cycle', Helvetica, Arial, 'Lucida Grande', sans-serif",
+      letterSpacing: "0.2em",
+      marginBottom: "20px",
+      paddingBottom: "10px",
+      textAlign: "center",
+      textTransform: "uppercase",
+    },
+    body2: {
+      textAlign: "center",
+      color: "#aaaaaa",
+    },
+  },
+});
 
 function App() {
   return (
     <>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/login" component={Login} />
-        <Route path="/signup" component={Signup} />
-      </Switch>
+      <MuiThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+        </Switch>
+      </MuiThemeProvider>
     </>
   );
 }
--- src/component/SignIn/Login.js
+++ src/component/SignIn/Login.js
@@ -1,10 +1,22 @@
 import React from "react";
 import Navbar from "../Navbar/Navbar";
+import { Typography, Grid, FormControl, InputLabel, Input } from "@material-ui/core";
 
 function Login() {
   return (
     <div>
       <Navbar />
+      <Grid xs={12} container justify="center">
+        <Grid xs={10} justify="center">
+          <Typography variant="h2">SignIn</Typography>
+          <Typography variant="body1">PERSONAL DETAILS</Typography>
+          <Typography variant="body2">Enter your email address and password to Login in your account.</Typography>
+          <FormControl>
+            <InputLabel htmlFor="username">Username</InputLabel>
+            <Input id="my-input" />
+          </FormControl>
+        </Grid>
+      </Grid>
     </div>
   );
 }
